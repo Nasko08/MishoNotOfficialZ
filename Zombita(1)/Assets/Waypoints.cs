ec885c4a66fc8dd74563a9a92fb88be9aea3522a
Atanas Tanev <atanastanev2008@gmail.com>
2024-01-05 20:57:54 +0200

    c#
    3tttuyt

---
+++ Zombita(1)/Assets/Waypoints.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] zombita;
+    private void Awake()
+    {
+        poin    
+        for (int i = 0; i < point.Lenght;i++) 
+        {
+
+        }
+
+
+    }
+
+
+
+}
